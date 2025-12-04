e2920fa89e26c1ac1cf0d1714e1ceb3bf5511d9c
LekhrajMahajan <lekhrajmahajan506@gmail.com>
2025-12-04 16:49:43 +0530

    errors solving

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PortfolioLM/'
+  base: '/PortfolioLM/',
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'EVAL' && warning.id.includes('lottie-web')) {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
 })
